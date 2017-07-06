a297ccfae99ab7b57f8b11c2cdf17c004cfadd73
Shatruanjay Kumar <darkspore3725@gmail.com>
2017-07-07 01:22:56 +0530

    Update countOf1sInBinaryArr.cpp

--- countOf1sInBinaryArr.cpp
+++ countOf1sInBinaryArr.cpp
@@ -3,21 +3,22 @@ int hash=2;
 using namespace std;
 int main(int argc, char **argv)
 {
-    vector <int> ab;
-    int n,i;
-    int cnt[hash];
+    //vector <int> ab;
+    //int n,i;
+    bool ab[] = {1, 1, 1, 1, 0, 0, 0};
+    int n = sizeof(ab)/sizeof(ab[0]);
+    int cnt[hash],i;
     for(i=0;i<hash;i++)
         cnt[i]=0;
-    cin>>n;
-    for(i=0;i<n;i++)
+    //cin>>n;
+    /*for(i=0;i<n;i++)
     {
     int p;
     cin>>p;
     ab.push_back(p);
-    }
+    }*/
     for(i=0;i<n;i++)
         cnt[(ab[i]%n)]++;
-    //for(i=0;i<hash;i++)
-        cout<<cnt[1]<<" ";    
+    cout<<cnt[1]<<" ";    
     return 0;
 }
